b0b07d3f595828cee7f945631b272602501d7355
Cara Chang <carachang27@Caras-MacBook-Pro.local>
2019-12-05 16:04:19 +0800

    debugging square

--- README.md
+++ README.md
@@ -9,13 +9,8 @@ are defined in the sketch, must be prefixed by `export` in order to be visible
 to p5.js. For example, `export function setup()` and `export function draw()` in
 the starter code.
 
-## History
-
-It was first used for the [p5.js particle
-workshop](https://www.notion.so/P5-js-Particle-Workshop-aba7992a689c457a8cca10e2f49e6a04)
-in the Fall 2019 Movement Practices course.
-
-Follow the instructions on that page for use.
+## Acknowledgement
+https://stackoverflow.com/questions/2142535/how-to-clear-the-canvas-for-redrawing
 
 ## License
 
--- sketch.js
+++ sketch.js
@@ -1,12 +1,55 @@
+import { onSensorData } from 'https://cdn.jsdelivr.net/npm/imu-tools@0/index.js'
+ 
+onSensorData(handleSensorData)
+
+let sensor = null
+let first = false
+let img 
+let x = 0
+let y = 0
+
 // Code in this function is run once, when the sketch is started.
 export function setup() {
-    createCanvas(windowWidth, windowHeight)
+    createCanvas(windowWidth-200, windowHeight)
+    img = loadImage('cup3.png')
 }
 
 // Code in this function is run once per frame. If it draws the same thing each
 // time, the sketch is a static image. If it draws something different on
 // different frames, the sketch is an animation.
 export function draw() {
-    fill('black')
-    rect(0, 0, width, height)
+    angleMode(DEGREES)
+    let angle = frameCount/5;
+    clear();
+    // rotateAbout(angle, 200, 100);
+    // image(img, x, y)
+    // fill('black')
+    // rect(0, 0, 20, 40)
+    const context = canvas.getContext('2d')
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    // image(img, x, y)
+    if (sensor !== null){
+        let a = sensor[0]
+        rotateAbout(a,img.width+150,img.height+150)
+
+        // rect(275,300,1,1)
+        image(img,625,135)
+        noFill()
+        rect(625,135,img.width,img.height)
+    }
+}
+
+function rotateAbout(angle, x, y){
+    translate(x,y);
+    rotate(angle);
+    translate(-x,-y);
+}
+
+function handleSensorData(data){
+    if (first === false){
+        console.info('sensor data:', data.euler)
+    }
+    first = true
+    sensor = data.euler
+        
 }
